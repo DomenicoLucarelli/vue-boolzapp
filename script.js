2885a7e023be24c99f024dba9dd074860600af99
DomenicoLucarelli <domyluca@gmail.com>
2023-03-16 23:45:40 +0100

    feat: milestone 5.1

--- script.js
+++ script.js
@@ -13,17 +13,21 @@ const { createApp } = Vue
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Hai portato a spasso il cane?',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Ricordati di stendere i panni',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 16:15:22',
                       message: 'Tutto fatto!',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false,
+                      isClick: false
                   }
               ],
           },
@@ -35,17 +39,20 @@ const { createApp } = Vue
                   {
                       date: '20/03/2020 16:30:00',
                       message: 'Ciao come stai?',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '20/03/2020 16:30:55',
                       message: 'Bene grazie! Stasera ci vediamo?',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   },
                   {
                       date: '20/03/2020 16:35:00',
                       message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   }
               ],
           },
@@ -57,17 +64,20 @@ const { createApp } = Vue
                   {
                       date: '28/03/2020 10:10:40',
                       message: 'La Marianna va in campagna',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   },
                   {
                       date: '28/03/2020 10:20:10',
                       message: 'Sicuro di non aver sbagliato chat?',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '28/03/2020 16:15:22',
                       message: 'Ah scusa!',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   }
               ],
           },
@@ -79,12 +89,14 @@ const { createApp } = Vue
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Lo sai che ha aperto una nuova pizzeria?',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Si, ma preferirei andare al cinema',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   }
               ],
           },
@@ -96,12 +108,14 @@ const { createApp } = Vue
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Ricordati di chiamare la nonna',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Va bene, stasera la sento',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   }
               ],
           },
@@ -113,17 +127,20 @@ const { createApp } = Vue
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Ciao Claudia, hai novità?',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Non ancora',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:51:00',
                       message: 'Nessuna nuova, buona nuova',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   }
               ],
           },
@@ -135,12 +152,14 @@ const { createApp } = Vue
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   }
               ],
           },
@@ -152,17 +171,20 @@ const { createApp } = Vue
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                      status: 'sent'
+                      status: 'sent',
+                      isClick: false
                   },
                   {
                       date: '10/01/2020 15:51:00',
                       message: 'OK!!',
-                      status: 'received'
+                      status: 'received',
+                      isClick: false
                   }
               ],
           }
@@ -183,7 +205,9 @@ const { createApp } = Vue
             status: 'received'
         },
 
-        name : ''
+        name : '',
+
+        
       
       }
     },
@@ -209,6 +233,21 @@ const { createApp } = Vue
     //   creo funzione che mi pusha nell'array al giusto indice un oggetto che è la risposta al mio messaggio
       receivedMessage(){
         this.contacts[this.activeIndex].messages.push(this.newReceivedMessage)
+      },
+            
+      isOnOrOff(index){
+        if(this.contacts[this.activeIndex].messages[index].isClick == false){
+            this.contacts[this.activeIndex].messages[index].isClick = true
+
+        }else{
+            this.contacts[this.activeIndex].messages[index].isClick = false
+        }
+
+        
+      },
+
+      deleteMessage(index){
+        this.contacts[this.activeIndex].messages.splice(index , 1)
       }
 
     },
